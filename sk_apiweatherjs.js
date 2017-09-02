114b69080d1bc3c535c539373e24775bcb5bce9e
Skprose <sangeethakperumal@gmail.com>
2017-09-02 15:24:29 -0600

    Adding Raw Weather API - Zipcode/id/lat&long

--- sk_apiweatherjs.js
+++ sk_apiweatherjs.js
@@ -1,54 +1,122 @@
-//global Variables
-var queryurl = "https:/api.openweathermap.org/data/2.5/forecast?APPID=";
-var apikey = "39e91c48d1b4f937beac8b5748619871";
-var param_1 = "&id=524901"
-$.ajax({
-  url: queryurl+apikey+param_1,
-  method:"GET"
-}).done(function(response){
-            console.log("The Weather API REsponse",response);
-            var len = response.list.length;
-            $("#total").text(len);
+  //global Variables
+function callweatherbycityid (cid)
+{
+                  var queryurl = "https:/api.openweathermap.org/data/2.5/forecast?APPID=";
+                  var apikey = "39e91c48d1b4f937beac8b5748619871";
+                  var param_1 = "&id="+cid;
+                  console.log('In function call by cityid');
+                  $.ajax({
+                    url: queryurl+apikey+param_1,
+                    method:"GET"
+                  }).done(function(response){
+                              console.log("The Weather API- by cityid REsponse",response);
+                              var len = response.list.length;
+                              $("#total1").text(len);
 
-            console.log("Total elements:",len);
-                for (var i = 0; i< len ; i++)
-                {
-                              /*  var dyrow = $("<tr>");
-                                var dycol1 = $("<th>");
-                                var dycol2 = $("<th>");
-                                var dycol3 = $("<th>");
-                                var dycol4 = $("<th>");
-                                $("#dycol1").append(`<br/> `);
-                                $("#dycol2").append(` `);
-                                $("#dycol3").append(` `);
-                                $("#dycol4").append(``); */
-                                var t1 = response.list[i].dt_txt;
-                                var t2 = response.list[i].weather[0].description;
-                                var t3 = response.list[i].main.temp_max;
-                              var t4 = response.list[i].main.temp_min;
-                                var t5 = response.list[i].wind.speed;
-                                var t6 = response.list[i].wind.deg;
-                                console.log('inside for');
-                      console.log('dat',t1);
-                      console.log('weather',t2);
-                      console.log('max temperature',t3);
-                      console.log('min temperature',t4);
-                      console.log('wind speed',t5);
-                      console.log('feels like',t6);
-                      var addrowitem = (
-                        `<tr>
-                             <th>${response.list[i].dt_txt}</th>
-                             <th>${response.list[i].weather[0].description}</th>
-                             <th>${response.list[i].main.temp_max}</th>
-                             <th>${response.list[i].main.temp_min}</th>
-                             <th>${response.list[i].wind.speed}</th>
-                             <th>${response.list[i].wind.deg}</th>
-                         </tr>`
-                      );
-                      $("#details").append(addrowitem);
+                              console.log("Total elements:",len);
+                                  for (var i = 0; i< len ; i++)
+                                  {
 
-                  }
-});
+
+                                        var addrowitem = (
+                                          `<tr>
+                                               <th>${response.list[i].dt_txt}</th>
+                                               <th>${response.list[i].weather[0].description}</th>
+                                                <th>${response.list[i].weather[0].icon}</th>
+                                               <th>${response.list[i].main.temp}</th>
+                                               <th>${response.list[i].wind.speed}</th>
+                                               <th>${response.list[i].wind.deg}</th>
+                                           </tr>`
+                                        );
+                                        $("#details1").append(addrowitem);
+
+                                    }
+                  });
+}
+function callweatherbyzipcode(zcode)
+{
+                  var queryurl = "https:/api.openweathermap.org/data/2.5/weather?APPID=";
+                  var apikey = "3d1a8b5789767e359e88b92160cb58fe";
+                  var param_1 = "&zip="+zcode;
+                  console.log('In function call by zipcode');
+                  $.ajax({
+                    url: queryurl+apikey+param_1,
+                    method:"GET"
+                  }).done(function(response){
+                              console.log("The Weather API - By Zipcode Response",response);
+                              var citydata = (`
+                                <tr>
+                                      <th>${response.name}</th>
+                                      <th>${response.sys.country}</th>
+                                     <th>${response.coord.lon}</th>
+                                     <th>${response.coord.lat}</th>
+                                      <th>${response.coord.lat}</th>
+                                 </tr>
+                               `);
+                              $("#total2").append(citydata);
+
+
+
+                                        var addrowitem = (
+                                          `<tr>
+
+                                               <th>${response.main.temp}</th>
+                                               <th>${response.main.temp}</th>
+                                               <th>${response.weather[0].main}</th>
+                                               <th>${response.weather[0].icon}</th>
+                                               <th>${response.wind.speed}</th>
+                                               <th>${response.wind.deg}</th>
+                                           </tr>`
+                                        );
+                                        $("#details2").append(addrowitem);
+
+
+                  });
+}
+
+function callweatherbylatlong(v_lat,v_long)
+{
+                  var queryurl = "https:/api.openweathermap.org/data/2.5/weather?APPID=";
+                  var apikey = "3d1a8b5789767e359e88b92160cb58fe";
+                  var param_1 = "&lat="+v_lat+"&lon="+v_long;
+                  console.log('In function call by lat & long');
+                  $.ajax({
+                    url: queryurl+apikey+param_1,
+                    method:"GET"
+                  }).done(function(response){
+                              console.log("The Weather API - by lat & long REsponse",response);
+                           var itemdetail = (`
+                              <tr>
+                                  <td>${response.name}</td>
+                                  <td>${response.sys.country}</td>
+                                  <td>${response.coord.lon}</td>
+                                  <td>${response.coord.lat}</td>
+                                  <td> </td>
+                              </tr>
+                             `)
+                              $("#total3").append(itemdetail);
+
+                                        var addrowitem = (
+                                          `<tr>
+                                               <td>${response.main.temp}</td>
+                                               <td>${response.weather[0].main}</td>
+                                               <td>${response.weather[0].description}</td>
+                                               <td>${response.weather[0].icon}</td>
+                                               <td>${response.wind.speed}</td>
+                                               <td>${response.wind.deg}</td>
+
+                                           </tr>`);
+
+                                        $("#details3").append(addrowitem);
+                  });
+}
+console.log('In JS file-');
+console.log('before function call city id');
+callweatherbycityid(2172797);
+console.log('before function call by zipcode');
+callweatherbyzipcode(80016);
+console.log('lat long function call');
+callweatherbylatlong(35,139);
 /*
 var qurl1 = "http://api.openweatresponse.list[i].main.temp_maxhermap.org/pollution/v1/co/0.0,10.0/2016-03-01Z.json?appid={39e91c48d1b4f937beac8b5748619871} 1 digit (~78km) ";
 console.log('min temperature',
